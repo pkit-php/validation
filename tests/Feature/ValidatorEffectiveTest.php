cc7e80c1937dc1541616820bcc9f7707b270f522
kau-gusto <kauaug.mo@gmail.com>
2024-02-04 08:59:00 -0300

    tests: tests modificados para o que valide o retorno esperado

--- tests/Feature/ValidatorEffectiveTest.php
+++ tests/Feature/ValidatorEffectiveTest.php
@@ -5,19 +5,22 @@ use Pkit\Validator\Validator;
 /* Validation effective */
 
 test('Validator by type successfully', function ($schema, $test) {
-    expect((new Validator($schema, true))
-        ->validate($test))->toBeTrue();
+    $test_for_validation = $test;
+    expect((new Validator($schema))
+        ->validate($test_for_validation))->toEqual($test_for_validation);
 })->with("types");
 
 test('Validator by keys successfully', function ($type, $test) {
+    $test_for_validation = ["key_$type" => $test];
     expect((new Validator([
         "key_$type" => $type
-    ], true))->validate(["key_$type" => $test]))->toBeTrue();
+    ]))->validate(["key_$type" => $test]))->toEqual($test_for_validation);
 })->with("types");
 
 test('Validator with @array especial key', function ($type, $test) {
-    expect((new Validator(["@array" => $type], true))
-        ->validate([$test, $test]))->toBeTrue();
+    $test_for_validation = [$test, $test, $test];
+    expect((new Validator(["@array" => $type]))
+        ->validate($test_for_validation))->toEqual($test_for_validation);
 })->with("types");
 
 dataset("types", [
@@ -38,7 +41,6 @@ dataset("types", [
     [2, 2],
     ["numeric", "1234"],
     ["null", null],
-    ["nan", acos(8)],
     [null, null],
     ["object", new stdClass],
     ["string", "string"],
